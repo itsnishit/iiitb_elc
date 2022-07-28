6333706582a8841cc213fcf6564d788f225b475c
itsnishit <86912339+itsnishit@users.noreply.github.com>
2022-07-28 10:23:25 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -1,1 +1,15 @@
+# read design
+
+read_verilog iiitb_elc.v
+
+# generic synthesis
+synth -top iiitb_elc
+
+# mapping to mycells.lib
+dfflibmap -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
+abc -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
+clean
+flatten
+# write synthesized design
+write_verilog -assert synth_iiitb_elc.v
 

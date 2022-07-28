dba2119dec8ac7de0e31515b3a4cbf74d7d5830a
itsnishit <86912339+itsnishit@users.noreply.github.com>
2022-07-28 10:30:04 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -6,10 +6,10 @@ read_verilog iiitb_elc.v
 synth -top iiitb_elc
 
 # mapping to mycells.lib
-dfflibmap -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
-abc -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
+dfflibmap -liberty /home/nishit/ElevatorController/sky130RTLDesignAndSynthesisWorkshop-main/my_lib/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/nishit/ElevatorController/sky130RTLDesignAndSynthesisWorkshop-main/my_lib/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 clean
 flatten
 # write synthesized design
-write_verilog -assert synth_iiitb_elc.v
+write_verilog -assert iiitb_elc_synth.v
 

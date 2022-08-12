d5947df92626665c8d52dfbd32b5c2b017a27f53
itsnishit <86912339+itsnishit@users.noreply.github.com>
2022-08-12 14:47:55 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -7,8 +7,10 @@ synth -top iiitb_elc
 
 # mapping to mycells.lib
 dfflibmap -liberty /home/nishit/iiitb_elc/iiitb_elc/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-abc -liberty /home/nishit/iiitb_elc/iiitb_elc/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/nishit/iiitb_elc/iiitb_elc/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
 clean
 flatten
 # write synthesized design
 write_verilog -noattr iiitb_elc_synth.v
+stat
+show
